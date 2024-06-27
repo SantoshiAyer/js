a21da01c07d132e7af7d2f43692be4e7833057a6
santoshi ayer <>
2024-06-27 14:28:12 +0545

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+const accountId = 6789;
+let accountEmail = "santu45@gmail.com";
+var accountPassword = "12345";
+accountCity = "ktm";
+
+// accountId = 2 // not allowed
+
+accountEmail = "sanu12@gmail.com";
+accountPassword = "2121";
+accountCity = "mnr";
+
+let accountState;
+// console.log(accountId);
+
+/*
+prefer not to use block scope and functional scop 
+*/
+console.table([
+accountId,
+accountEmail,
+accountPassword,
+accountCity,
+accountState,
+]);
+
+// 43:54
